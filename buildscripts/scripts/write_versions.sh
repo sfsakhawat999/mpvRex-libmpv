4aa058dae492070bf31ffaa79c3f84998c77f535
sfsakhawat999 <sfsakhawat999@gmail.com>
2026-07-11 22:25:59 +0600

    Fix build path typo in write_versions.sh

--- buildscripts/scripts/write_versions.sh
+++ buildscripts/scripts/write_versions.sh
@@ -8,16 +8,16 @@ if [ -z "$ARCH" ]; then
     ARCH="arm64"
 fi
 
-MPV_VERSION=$(cat deps/mpv/_build${ARCH}/common/version.h | grep "#define VERSION" | cut -d '"' -f 2)
-LIBPLACEBO_VERSION=$(cat deps/libplacebo/_build${ARCH}/src/version.h | grep "#define BUILD_VERSION" | cut -d '"' -f 2)
+MPV_VERSION=$(cat deps/mpv/_build_${ARCH}/common/version.h | grep "#define VERSION" | cut -d '"' -f 2)
+LIBPLACEBO_VERSION=$(cat deps/libplacebo/_build_${ARCH}/src/version.h | grep "#define BUILD_VERSION" | cut -d '"' -f 2)
 FFMPEG_VERSION=$(echo $(cd deps/ffmpeg/ && git rev-parse --short HEAD))
 
 # get build date from compiled object file
-START_RODATA=0x$(readelf deps/mpv/_build${ARCH}/libmpv.so.p/common_version.c.o -S | grep .rodata | cut -d ' ' -f 27)
-START=0x$(readelf deps/mpv/_build${ARCH}/libmpv.so.p/common_version.c.o -s | grep mpv_builddate | cut -d ' ' -f 7)
-SIZE=$(readelf deps/mpv/_build${ARCH}/libmpv.so.p/common_version.c.o -s | grep mpv_builddate | cut -d ' ' -f 11)
+START_RODATA=0x$(readelf deps/mpv/_build_${ARCH}/libmpv.so.p/common_version.c.o -S | grep .rodata | cut -d ' ' -f 27)
+START=0x$(readelf deps/mpv/_build_${ARCH}/libmpv.so.p/common_version.c.o -s | grep mpv_builddate | cut -d ' ' -f 7)
+SIZE=$(readelf deps/mpv/_build_${ARCH}/libmpv.so.p/common_version.c.o -s | grep mpv_builddate | cut -d ' ' -f 11)
 SKIP=$(($START_RODATA + $START - 1))
-dd if=deps/mpv/_build${ARCH}/libmpv.so.p/common_version.c.o of=date.txt bs=1 skip=$SKIP count=$SIZE
+dd if=deps/mpv/_build_${ARCH}/libmpv.so.p/common_version.c.o of=date.txt bs=1 skip=$SKIP count=$SIZE
 DATE=$(cat date.txt)
 rm date.txt
 
